e811d3f0af3f591eb4accd55ce1e071d7f228190
Cory Charlton <ccharlton@pobox.com>
2019-05-31 10:56:51 -0700

    Initial commit

---
+++ src/AndroidStringResourcesSorter/FormMain.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace AndroidStringResourcesSorter
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this._labelResourcesFolder = new System.Windows.Forms.Label();
+            this._textBoxResourcesFolder = new System.Windows.Forms.TextBox();
+            this._buttonBrowse = new System.Windows.Forms.Button();
+            this._textBoxSourceContent = new System.Windows.Forms.TextBox();
+            this._textBoxSortedContent = new System.Windows.Forms.TextBox();
+            this._splitContainerContent = new System.Windows.Forms.SplitContainer();
+            this._buttonSave = new System.Windows.Forms.Button();
+            this._labelResults = new System.Windows.Forms.Label();
+            this._listBoxStringResourceFiles = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this._splitContainerContent)).BeginInit();
+            this._splitContainerContent.Panel1.SuspendLayout();
+            this._splitContainerContent.Panel2.SuspendLayout();
+            this._splitContainerContent.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // _labelResourcesFolder
+            // 
+            this._labelResourcesFolder.AutoSize = true;
+            this._labelResourcesFolder.Location = new System.Drawing.Point(12, 17);
+            this._labelResourcesFolder.Name = "_labelResourcesFolder";
+            this._labelResourcesFolder.Size = new System.Drawing.Size(90, 13);
+            this._labelResourcesFolder.TabIndex = 0;
+            this._labelResourcesFolder.Text = "Resources folder:";
+            // 
+            // _textBoxResourcesFolder
+            // 
+            this._textBoxResourcesFolder.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this._textBoxResourcesFolder.Location = new System.Drawing.Point(108, 14);
+            this._textBoxResourcesFolder.Name = "_textBoxResourcesFolder";
+            this._textBoxResourcesFolder.ReadOnly = true;
+            this._textBoxResourcesFolder.Size = new System.Drawing.Size(599, 20);
+            this._textBoxResourcesFolder.TabIndex = 1;
+            this._textBoxResourcesFolder.TabStop = false;
+            // 
+            // _buttonBrowse
+            // 
+            this._buttonBrowse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this._buttonBrowse.Location = new System.Drawing.Point(713, 12);
+            this._buttonBrowse.Name = "_buttonBrowse";
+            this._buttonBrowse.Size = new System.Drawing.Size(75, 23);
+            this._buttonBrowse.TabIndex = 2;
+            this._buttonBrowse.Text = "&Browse";
+            this._buttonBrowse.UseVisualStyleBackColor = true;
+            this._buttonBrowse.Click += new System.EventHandler(this.ButtonBrowse_Click);
+            // 
+            // _textBoxSourceContent
+            // 
+            this._textBoxSourceContent.Dock = System.Windows.Forms.DockStyle.Fill;
+            this._textBoxSourceContent.Location = new System.Drawing.Point(0, 0);
+            this._textBoxSourceContent.Multiline = true;
+            this._textBoxSourceContent.Name = "_textBoxSourceContent";
+            this._textBoxSourceContent.ReadOnly = true;
+            this._textBoxSourceContent.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this._textBoxSourceContent.Size = new System.Drawing.Size(386, 278);
+            this._textBoxSourceContent.TabIndex = 0;
+            this._textBoxSourceContent.TabStop = false;
+            this._textBoxSourceContent.WordWrap = false;
+            // 
+            // _textBoxSortedContent
+            // 
+            this._textBoxSortedContent.Dock = System.Windows.Forms.DockStyle.Fill;
+            this._textBoxSortedContent.Location = new System.Drawing.Point(0, 0);
+            this._textBoxSortedContent.Multiline = true;
+            this._textBoxSortedContent.Name = "_textBoxSortedContent";
+            this._textBoxSortedContent.ReadOnly = true;
+            this._textBoxSortedContent.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this._textBoxSortedContent.Size = new System.Drawing.Size(385, 278);
+            this._textBoxSortedContent.TabIndex = 0;
+            this._textBoxSortedContent.TabStop = false;
+            this._textBoxSortedContent.WordWrap = false;
+            // 
+            // _splitContainerContent
+            // 
+            this._splitContainerContent.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this._splitContainerContent.Location = new System.Drawing.Point(12, 128);
+            this._splitContainerContent.Name = "_splitContainerContent";
+            // 
+            // _splitContainerContent.Panel1
+            // 
+            this._splitContainerContent.Panel1.Controls.Add(this._textBoxSourceContent);
+            // 
+            // _splitContainerContent.Panel2
+            // 
+            this._splitContainerContent.Panel2.Controls.Add(this._textBoxSortedContent);
+            this._splitContainerContent.Size = new System.Drawing.Size(776, 278);
+            this._splitContainerContent.SplitterDistance = 386;
+            this._splitContainerContent.SplitterWidth = 5;
+            this._splitContainerContent.TabIndex = 4;
+            this._splitContainerContent.TabStop = false;
+            // 
+            // _buttonSave
+            // 
+            this._buttonSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this._buttonSave.Location = new System.Drawing.Point(713, 415);
+            this._buttonSave.Name = "_buttonSave";
+            this._buttonSave.Size = new System.Drawing.Size(75, 23);
+            this._buttonSave.TabIndex = 6;
+            this._buttonSave.Text = "&Save";
+            this._buttonSave.UseVisualStyleBackColor = true;
+            this._buttonSave.Click += new System.EventHandler(this.ButtonSave_Click);
+            // 
+            // _labelResults
+            // 
+            this._labelResults.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this._labelResults.Location = new System.Drawing.Point(12, 420);
+            this._labelResults.Name = "_labelResults";
+            this._labelResults.Size = new System.Drawing.Size(695, 13);
+            this._labelResults.TabIndex = 5;
+            // 
+            // _listBoxStringResourceFiles
+            // 
+            this._listBoxStringResourceFiles.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this._listBoxStringResourceFiles.FormattingEnabled = true;
+            this._listBoxStringResourceFiles.Location = new System.Drawing.Point(12, 40);
+            this._listBoxStringResourceFiles.Name = "_listBoxStringResourceFiles";
+            this._listBoxStringResourceFiles.Size = new System.Drawing.Size(776, 82);
+            this._listBoxStringResourceFiles.TabIndex = 3;
+            this._listBoxStringResourceFiles.SelectedIndexChanged += new System.EventHandler(this.ListBoxStringResourceFiles_SelectedIndexChanged);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this._listBoxStringResourceFiles);
+            this.Controls.Add(this._labelResults);
+            this.Controls.Add(this._buttonSave);
+            this.Controls.Add(this._splitContainerContent);
+            this.Controls.Add(this._buttonBrowse);
+            this.Controls.Add(this._textBoxResourcesFolder);
+            this.Controls.Add(this._labelResourcesFolder);
+            this.Name = "FormMain";
+            this.Text = "String Resources Sorter";
+            this._splitContainerContent.Panel1.ResumeLayout(false);
+            this._splitContainerContent.Panel1.PerformLayout();
+            this._splitContainerContent.Panel2.ResumeLayout(false);
+            this._splitContainerContent.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this._splitContainerContent)).EndInit();
+            this._splitContainerContent.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label _labelResourcesFolder;
+        private System.Windows.Forms.TextBox _textBoxResourcesFolder;
+        private System.Windows.Forms.Button _buttonBrowse;
+        private System.Windows.Forms.TextBox _textBoxSourceContent;
+        private System.Windows.Forms.TextBox _textBoxSortedContent;
+        private System.Windows.Forms.SplitContainer _splitContainerContent;
+        private System.Windows.Forms.Button _buttonSave;
+        private System.Windows.Forms.Label _labelResults;
+        private System.Windows.Forms.ListBox _listBoxStringResourceFiles;
+    }
+}
+
---
+++ src/AndroidStringResourcesSorter/FormMain.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace AndroidStringResourcesSorter
+{
+    public partial class FormMain : Form
+    {
+        private SortedDictionary<string, XmlNode> _stringArrayNodes;
+        private SortedDictionary<string, XmlNode> _stringNodes;
+
+        private void ButtonBrowse_Click(object sender, EventArgs e)
+        {
+            _buttonSave.Enabled = false;
+
+            // TODO: Should remeber / restore the last selected directory
+            using (var dialog = new FolderBrowserDialog())
+            {
+                // NOTE: For testing purposes
+                //dialog.SelectedPath = @"C:\src\personal\SmokingLog\common\src\main\res";
+                dialog.ShowNewFolderButton = false;
+
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    ResetContent();
+
+                    _labelResults.Text = string.Empty;
+                    _listBoxStringResourceFiles.Items.Clear();
+                    _textBoxResourcesFolder.Text = dialog.SelectedPath;
+
+                    var directories = Directory.GetDirectories(_textBoxResourcesFolder.Text, "values*");
+                    foreach (var directory in directories)
+                    {
+                        var files = Directory.GetFiles(directory, "strings.xml");
+                        foreach (var file in files)
+                        {
+                            _listBoxStringResourceFiles.Items.Add(file);
+                        }
+                    }
+
+                    if (_listBoxStringResourceFiles.Items.Count > 0)
+                    {
+                        _listBoxStringResourceFiles.SelectedIndex = 0;
+                        _listBoxStringResourceFiles.Focus();
+                    }
+                }
+            }
+        }
+
+        private void ButtonSave_Click(object sender, EventArgs e)
+        {
+            _listBoxStringResourceFiles.Enabled = false;
+
+            var filename = _listBoxStringResourceFiles.SelectedItem as string;
+
+            if (!string.IsNullOrWhiteSpace(filename))
+            {
+                File.WriteAllText(filename, _textBoxSortedContent.Text);
+
+                _buttonSave.Enabled = false;
+                _labelResults.Text = "Successfully saved sorted file";
+            }
+            else
+            {
+                _labelResults.Text = "Invalid filename";
+            }
+
+            _listBoxStringResourceFiles.Enabled = true;
+        }
+
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void ListBoxStringResourceFiles_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SortStringResources(_listBoxStringResourceFiles.SelectedItem as string);
+        }
+
+        private void ResetContent()
+        {
+            _textBoxSortedContent.Text = string.Empty;
+            _textBoxSourceContent.Text = string.Empty;
+
+            _stringArrayNodes = new SortedDictionary<string, XmlNode>();
+            _stringNodes = new SortedDictionary<string, XmlNode>();
+        }
+
+        private void SortStringResources(string fileName)
+        {
+            if (string.IsNullOrWhiteSpace(fileName))
+            {
+                return;
+            }
+
+            ResetContent();
+
+            _listBoxStringResourceFiles.Enabled = false;
+
+            var sourceContent = new StringBuilder();
+            using (var reader = File.OpenText(fileName))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var currentLine = reader.ReadLineAsync().Result;
+                    if (!string.IsNullOrWhiteSpace(currentLine))
+                    {
+                        sourceContent.AppendLine(currentLine);
+                    }
+                }
+            }
+
+            _textBoxSourceContent.Text = sourceContent.ToString().Trim();
+
+            var sourceXml = new XmlDocument();
+            sourceXml.LoadXml(_textBoxSourceContent.Text);
+
+            var resourcesElement = sourceXml.GetElementsByTagName("resources")[0];
+
+            var stringArrays = resourcesElement.SelectNodes("string-array");
+            if (stringArrays != null)
+            {
+                foreach (XmlNode node in stringArrays)
+                {
+                    if (node?.Attributes == null)
+                    {
+                        continue;
+                    }
+
+                    _stringArrayNodes.Add(node.Attributes["name"].Value, node);
+                }
+            }
+
+            var strings = resourcesElement.SelectNodes("string");
+            if (strings != null)
+            {
+                foreach (XmlNode node in strings)
+                {
+                    if (node?.Attributes == null)
+                    {
+                        continue;
+                    }
+
+                    _stringNodes.Add(node.Attributes["name"].Value, node);
+                }
+            }
+
+            resourcesElement.RemoveAll();
+
+            foreach (var element in _stringNodes.Values)
+            {
+                resourcesElement.AppendChild(element);
+            }
+
+            foreach (var element in _stringArrayNodes.Values)
+            {
+                resourcesElement.AppendChild(element);
+            }
+
+            var settings = new XmlWriterSettings
+            {
+                Indent = true,
+                IndentChars = "    ",
+                Encoding = Encoding.UTF8
+            };
+
+            using (var stringWriter = new StringWriter())
+            using (var xmlTextWriter = XmlWriter.Create(stringWriter, settings))
+            {
+                sourceXml.WriteTo(xmlTextWriter);
+                xmlTextWriter.Flush();
+
+                _textBoxSortedContent.Text = stringWriter.GetStringBuilder().ToString();
+            }
+
+            _buttonSave.Enabled = true;
+            _labelResults.Text = $"Source lines: {_textBoxSourceContent.Lines.Length} | Target lines: {_textBoxSortedContent.Lines.Length}";
+            _listBoxStringResourceFiles.Enabled = true;
+            _listBoxStringResourceFiles.Focus();
+        }
+    }
+}
